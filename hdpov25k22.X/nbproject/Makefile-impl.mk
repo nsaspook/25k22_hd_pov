915df1ec518c61b70a821e0b046d76a08db37ef0
nsaspook <nsaspook@nsaspook.com>
2023-05-05 08:26:51 -0700

    up

--- hdpov25k22.X/nbproject/Makefile-impl.mk
+++ hdpov25k22.X/nbproject/Makefile-impl.mk
@@ -27,11 +27,11 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 PROJECTNAME=hdpov25k22.X
 
 # Active Configuration
-DEFAULTCONF=default
+DEFAULTCONF=45k22
 CONF=${DEFAULTCONF}
 
 # All Configurations
-ALLCONFS=default 
+ALLCONFS=default 45k22 
 
 
 # build
@@ -46,12 +46,14 @@ ALLCONFS=default
 # clobber
 .clobber-impl: .clobber-pre .depcheck-impl
 	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=default clean
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=45k22 clean
 
 
 
 # all
 .all-impl: .all-pre .depcheck-impl
 	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=default build
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=45k22 build
 
 
 
--- hdpov25k22.X/nbproject/Makefile-variables.mk
+++ hdpov25k22.X/nbproject/Makefile-variables.mk
@@ -8,3 +8,7 @@ CND_BASEDIR=`pwd`
 CND_ARTIFACT_DIR_default=dist/default/production
 CND_ARTIFACT_NAME_default=hdpov25k22.X.production.hex
 CND_ARTIFACT_PATH_default=dist/default/production/hdpov25k22.X.production.hex
+# 45k22 configuration
+CND_ARTIFACT_DIR_45k22=dist/45k22/production
+CND_ARTIFACT_NAME_45k22=hdpov25k22.X.production.hex
+CND_ARTIFACT_PATH_45k22=dist/45k22/production/hdpov25k22.X.production.hex
